bbc18148078e0321b10407af776a9d65d0bc3589
Rob Sessink <rob.sessink@yucca.io>
2021-12-14 21:28:39 +0100

    add advent of code day-3: part 2

--- day-3/Diagnostics.fs
+++ day-3/Diagnostics.fs
@@ -3,26 +3,72 @@ module Diagnostics
 open System
 open System.IO
 
-let calculateRate diagnostics func =
+let calculateRate (diagnostics: char list list) func =
     diagnostics
+    |> List.transpose
     |> List.map (List.countBy id)
     |> List.map (func snd)
-    |> List.fold (fun s (c, _) -> (s + string c)) ""
+    |> List.fold (fun s (c, _) -> c :: s) []
+    |> List.rev
 
-let toDecimal rate = Convert.ToInt32(rate, 2)
+let toDecimalString (rate: char list) = Convert.ToInt32(String.Concat rate, 2)
+
+let maxByOrWhenEqual value1 value2 : (char * int) =
+    match Operators.compare (snd value1) (snd value2) with
+    | 1 -> value1
+    | -1 -> value2
+    | 0 -> ('1', 1)
+    | a -> invalidArg "compare values" $"Unknown comparison values: {a}"
+
+let minByOrWhenEqual value1 value2 : (char * int) =
+    match Operators.compare (snd value1) (snd value2) with
+    | 1 -> value2
+    | -1 -> value1
+    | 0 -> ('0', 0)
+    | a -> invalidArg "compare values" $"Unknown comparison values: {a}"
+
+let cmpMax _ list = list |> List.reduce maxByOrWhenEqual
+
+let cmpMin _ list = list |> List.reduce minByOrWhenEqual
+
+let rec calculateRating cmp bit (diagnostics: char list list) =
+    let tps = List.transpose diagnostics
+
+    match diagnostics with
+    | d when d.Length = 1 -> diagnostics |> List.concat
+    | _ ->
+        let m = tps.[bit] |> List.countBy id |> cmp snd
+
+        let diagnostics' =
+            diagnostics
+            |> List.filter (fun d -> d.[bit] = fst m)
+
+        calculateRating cmp (bit + 1) diagnostics'
 
 let calculateGamma diagnostics = calculateRate diagnostics List.maxBy
 
 let calculateEpsilon diagnostics = calculateRate diagnostics List.minBy
 
-let toDiagnostics report =
-    report |> List.map Seq.toList |> List.transpose
+let calculateOxygenGenerator diagnostics = diagnostics |> calculateRating cmpMax 0
+
+let calculateC02Scrubber diagnostics = diagnostics |> calculateRating cmpMin 0
+
+let toDiagnostics report = report |> List.map Seq.toList
 
 let readReport file = File.ReadLines file |> Seq.toList
 
-let calculatePowerConsumption file =
+let calculateByCombinedRateFunctions file rateFuncs =
     let diagnostics = readReport file |> toDiagnostics
 
-    [ calculateGamma; calculateEpsilon ]
-    |> List.map (fun f -> f diagnostics |> toDecimal)
+    rateFuncs
+    |> List.map (fun f -> f diagnostics |> toDecimalString)
     |> List.reduce (*)
+
+let calculatePowerConsumption file =
+    [ calculateGamma; calculateEpsilon ]
+    |> calculateByCombinedRateFunctions file
+
+let calculateLifeSupportRating file =
+    [ calculateOxygenGenerator
+      calculateC02Scrubber ]
+    |> calculateByCombinedRateFunctions file
--- day-3/DiagnosticsTests.fs
+++ day-3/DiagnosticsTests.fs
@@ -15,3 +15,13 @@ let ``Part 1: Captain, reading out the diagnositics-test.txt, we don't have powe
 let ``Part 1: Captain, reading out the diagnositics.txt, we don't have power enough!`` () =
     calculatePowerConsumption "diagnostics.txt"
     |> should equal 3847100
+
+[<Fact>]
+let ``Part 2: Captain, reading out the diagnositics-test.txt, we are running out of oxygen!`` () =
+    calculateLifeSupportRating "diagnostics-test.txt"
+    |> should equal 230
+    
+[<Fact>]
+let ``Part 2: Captain, reading out the diagnositics.txt, we are running out of oxygen!`` () =
+    calculateLifeSupportRating "diagnostics.txt"
+    |> should equal 4105235    
